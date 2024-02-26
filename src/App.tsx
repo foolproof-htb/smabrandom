4f7c0761d992b989571f0199ba696206470dbde0
foolproof-htb <fujisawa.htb@gmail.com>
2024-02-26 19:35:30 +0900

    :sparkles: ヘッダーとフッターを設定した

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,15 @@ const { Header, Content, Footer } = Layout
 function App() {
   return (
     <Layout>
-      <Header>ヘッダー</Header>
+      <Header style={{ color: '#fff', fontSize: 24 }}>SmaB-random</Header>
       <Content
         style={{ padding: '12px', margin: '12px', backgroundColor: 'white' }}
       >
         <ContentBody />
       </Content>
-      <Footer>フッター</Footer>
+      <Footer>
+        created by <a href="https://github.com/foolproof-htb">foolproof-htb</a>
+      </Footer>
     </Layout>
   )
 }
